14fb01369c8eb05791286cf8a7d0167f7a49e4f7
otsoweckstrom <42272286+otsoweckstrom@users.noreply.github.com>
2022-12-08 03:28:38 +0200

    add backend architecture

--- index.js
+++ index.js
@@ -1,21 +1,67 @@
-const express = require('express');
-const app = express();
-const mysql = require('mysql');
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const mysql = require('mysql')
+const cors = require('cors')
 
-const db = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 'password',
-  databases: 'moviedatabase',
-});
+//create three separate database connections
+const db1 = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'moviedatabase1',
+})
+const db2 = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'moviedatabase2',
+})
+const db3 = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'moviedatabase3',
+})
 
-app.get('/', (req, res) => {
-  const sqlInsert = "INSERT INTO movies (moviename) VALUES ('Shrek');";
-  db.query(sqlInsert, (req, result) => {
-    res.send('hello world');
-  });
-});
+app.use(cors())
+app.use(express.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+//Get the movies fix table name, one database for each country queryed by /[country]
+app.get('/api/get/finland', (req, res) => {
+    const sqlSelect = 'SELECT * FROM movie'
+    db1.query(sqlSelect, (err, result) => {
+        res.send(result)
+        console.log(result)
+        console.log(err)
+        console.log('finland')
+    })
+})
+app.get('/api/get/spain', (req, res) => {
+    const sqlSelect = 'SELECT * FROM movie'
+    db2.query(sqlSelect, (err, result) => {
+        res.send(result)
+        console.log(result)
+        console.log(err)
+        console.log('spain')
+    })
+})
+app.get('/api/get/usa', (req, res) => {
+    const sqlSelect = 'SELECT * FROM movie'
+    db3.query(sqlSelect, (err, result) => {
+        res.send(result)
+        console.log(result)
+        console.log(err)
+        console.log('usa')
+    })
+})
+
+//posting left unfinished since no functionality needs it
+app.post('/api/insert', (req, res) => {
+    db1.query()
+})
 
 app.listen(3001, () => {
-  console.log('running on port 3001');
-});
+    console.log('running on port 3001')
+})
